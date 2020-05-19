c22147019cee38e1efb451c6a2ab37e954e1ee8c
Philipp Wagner <pwag86@googlemail.com>
2020-05-19 21:21:47 +0200

    Update Status Values without complete DataSource reload

--- Frontend/search-app/src/app/components/document-status/document-status.component.ts
+++ Frontend/search-app/src/app/components/document-status/document-status.component.ts
@@ -1,12 +1,12 @@
 
-import { Component, OnInit, HostListener, OnDestroy } from '@angular/core';
+import { Component, OnInit, HostListener, OnDestroy, ChangeDetectorRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '@environments/environment';
 import { MatTableDataSource } from '@angular/material/table';
 import { SelectionModel } from '@angular/cdk/collections';
 import { DocumentStatus } from '@app/app.model';
-import { catchError, concatMap, mergeMap, toArray, tap } from 'rxjs/operators';
-import { of, from, Subject } from 'rxjs';
+import { catchError, concatMap, mergeMap, toArray, tap, takeUntil } from 'rxjs/operators';
+import { of, from, Subject, interval } from 'rxjs';
 
 @Component({
   selector: 'app-document-status',
@@ -23,28 +23,50 @@ export class DocumentStatusComponent implements OnInit, OnDestroy {
   dataSource = new MatTableDataSource<DocumentStatus>();
   selection = new SelectionModel<DocumentStatus>(true, []);
 
-  constructor(private httpClient: HttpClient) {
+  constructor(private httpClient: HttpClient, private changeDetectorRefs: ChangeDetectorRef) {
 
   }
 
   ngOnInit(): void {
+    interval(5000)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(() => this.reloadStatusValues());
+
     this.reloadDataTable();
   }
 
   reloadDataTable() {
     this.selection.clear();
-    
-    this.isDataSourceLoading = true
 
     this.httpClient
       .get<DocumentStatus[]>(`${environment.apiUrl}/status`)
       .pipe(
         catchError(() => of<DocumentStatus[]>([])))
       .subscribe(data => {
-        this.isDataSourceLoading = false;
         this.dataSource.data = data;
       });
   }
+
+  reloadStatusValues() {
+    this.httpClient
+      .get<DocumentStatus[]>(`${environment.apiUrl}/status`)
+      .pipe(
+        catchError(() => of<DocumentStatus[]>([])))
+      .subscribe(data => {
+
+        const status = new Map(data.map(i => [i.id, i.status]));
+
+        this.dataSource.data
+          .forEach(row => {
+            if (status.has(row.id)) {
+              row.status = status.get(row.id);
+            }
+          });
+
+        this.changeDetectorRefs.detectChanges();
+      });
+  }
+
 
   /** Whether the number of selected elements matches the total number of rows. */
   isAllSelected() {
